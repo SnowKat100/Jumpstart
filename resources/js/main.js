d46fd98c9988a7b196c1871016412e9393166f30
KATHERINE SNOW <Katy@KATHERINEs-iMac.local>
2018-06-21 12:31:13 +0200

    complete project

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    /* Slick carousel settings */
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
